4136e2995e4e0ce4b6939577cf453aff95f4a5c2
JeromeBern <78410684+jeromebernante@users.noreply.github.com>
2024-03-01 12:43:30 +0800

    Update edit.php

--- edit.php
+++ edit.php
@@ -7,7 +7,14 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
+<?php 
+    if (isset($_GET['edit'])) {
+        $userid = $_GET['edit'];
+        $username = $_GET['username'];
+    }
+    
 
+?>
 <body>
     <div class="container">
         <header class="d-flex align-items-center">
@@ -20,10 +27,10 @@
             <form>
                 <div class="mb-3">
                     <label for="" class="form-label">Username</label>
-                    <input type="email" class="form-control" id="" >
+                    <input type="email" class="form-control" id="" value="<?php echo $username?>">
                 </div>
                 <div class="mb-3">
-                    <label for="" class="form-label">Password</label>
+                    <label for="" class="form-label">New Password</label>
                     <input type="password" class="form-control" id="">
                 </div>
                 <button type="submit" class="btn btn-primary">Save</button>
