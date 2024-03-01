8a49b18fc6df958d1ee8c6858276a1670e85db6d
JeromeBern <78410684+jeromebernante@users.noreply.github.com>
2024-03-01 13:30:01 +0800

    Update index.php

--- index.php
+++ index.php
@@ -13,52 +13,51 @@ include('connection.php')
 </head>
 <body>
 
-<nav class="navbar navbar-light bg-light">
-  <div class="container">
-    <a class="navbar-brand" href="#">BERNANTE JEROME</a>
-  </div>
-</nav>
-
-
-<?php 
-    if (isset($_GET['edit'])) {
-        echo $_GET['edit'];
-    }
-?>
-
-
-
 <div class="container">
-    <div class="card">
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th scope="col">UserID</th>
-                    <th scope="col">Username</th>
-                    <th scope="col">Password</th>
-                    <th scope="col">Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                    while ($row = mysqli_fetch_array($select_all))
-                    {
-                ?>
-                    <tr>
-                        <td scope="row"><?php echo $row['userid']?></td>
-                        <td><?php echo $row['username']?></td>
-                        <td><?php echo $row['password']?></td>
-                        <td>
-                            <a href="edit.php?edit=<?php echo $row['userid']?>" class="btn btn-primary">EDIT</a>
-                            <a href="index.php?delete=<?php echo $row['userid']?>" class="btn btn-danger">DEL</a>
-                        </td>
-                    </tr>
-                <?php
-                    }
-                ?>
-            </tbody>
-        </table>
-    </div>
+        <header class="d-flex align-items-center">
+            <h1>JEROME BERNANTE</h1>
+            <div class="ms-auto">
+                <a href="index.php" class="btn btn-danger">LOGOUT</a>
+            </div>
+        </header>
+        <nav>
+            <a href="add.php" class="btn btn-primary mb-3">ADD</a>
+
+        </nav>
+        <main>
+            <div class="card shadow">
+                <table class="table table-striped">
+                    <thead>
+                        <tr>
+                            <th scope="col">UserID</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Password</th>
+                            <th scope="col">Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while ($row = mysqli_fetch_array($select_all))
+                            {
+                        ?>
+                            <tr>
+                                <td scope="row"><?php echo $row['userid']?></td>
+                                <td><?php echo $row['username']?></td>
+                                <td><?php echo $row['password']?></td>
+                                <td>
+                                    <a href="edit.php?edit=<?php echo $row['userid']?>&username=<?php echo $row['username']?>" class="btn btn-primary">EDIT</a>
+                                    <a href="delete.php?id=<?php echo $row['userid']?>" class="btn btn-danger">DEL</a>
+                                </td>
+                            </tr>
+                        <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </main>
+
+
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
