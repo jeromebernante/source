c3a1a068e9fd48cbcb6951347b7d1011a0bec1df
JeromeBern <78410684+jeromebernante@users.noreply.github.com>
2024-03-01 13:29:37 +0800

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,1 +1,7 @@
+<?php 
+    include('connection.php');
 
+    $userid = $_GET['id'];
+    $sql = "DELETE FROM tbl_user WHERE userid = $userid";
+    mysqli_query($conn, $sql);
+    header('Location: index.php');
